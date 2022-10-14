36ad8f39b702de5941dffd397e2b83e80bfa1345
omkar1204 <omahirkar008@gmail.com>
2022-10-14 20:57:35 +0530

    Old Package

--- bootcoding_java/src/com/bootcoding/collection/MarkerCollection.java
+++ bootcoding_java/src/com/bootcoding/collection/MarkerCollection.java
@@ -4,30 +4,44 @@ import com.bootcoding.oops.encapsulation.Marker2;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MarkerCollection {
+
+
     Map<Long, Marker2> markerStore=new HashMap<>();
     public void addMarker (Marker2 m){
         markerStore.put(m.getId(),m);
     }
     public void printMarkers(){
         for (Marker2 m : markerStore.values()){
-            System.out.println("Id" + m.getId());
-            System.out.println("Company" + m.getCompany());
-            System.out.println("Colour" + m.getColour());
-            System.out.println("Type" + m.getType());
-            System.out.println("Price" + m.getPrice());
+            System.out.println(" Id " + m.getId());
+            System.out.println(" Company " + m.getCompany());
+            System.out.println(" Colour " + m.getColour());
+            System.out.println(" Type " + m.getType());
+            System.out.println(" Price " + m.getPrice());
         }
     }
 
+
     public static void main(String[] args) {
+        int randomColour;
         MarkerCollection MarkerCollection = new MarkerCollection();
+
+            Random random = new Random();
+
+
+
         for (int i=1;i<10;i++){
+
+            String [] Colours={"BLACK","BLUE","RED","GREEN"};
+            randomColour = random.nextInt(Colours.length);
+
             Marker2 m= new Marker2();
             m.setId(i);
             m.setCompany("Camlin" + i);
             m.setType("White" + i);
-            m.setColour("Blue");
+            m.setColour(Colours[randomColour]);
             m.setPrice((i+100)*2.5);
             MarkerCollection.addMarker(m);
 
