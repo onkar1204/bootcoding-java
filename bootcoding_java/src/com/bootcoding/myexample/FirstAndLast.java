286a1b2a69b3d8acd2a37403ed9595df2b2ce58c
omkar1204 <omahirkar008@gmail.com>
2023-01-11 22:08:29 +0530

    Method Calling

--- bootcoding_java/src/com/bootcoding/myexample/FirstAndLast.java
+++ bootcoding_java/src/com/bootcoding/myexample/FirstAndLast.java
@@ -2,22 +2,14 @@ package com.bootcoding.myexample;
 
     public class FirstAndLast {
     public static void main(String args[]){
-
     int num = 456868768;
     int first = 0;
     int last = num % 10;
-
-
     while(num != 0){
         first = num; // 456868768, 45686876, 4568687, 456868, 45686, 4568, 456, 45, 4
         num = num/10; // 45686876, 4568687, 456868, 45686, 4568, 456, 45, 4, 0
- 
- 
- 
     }
         System.out.println("First = " + first);
         System.out.println("Last = " + last);
-
-
     }
     }
