2697c48794bde3f36adc2285ee41067710752454
omkar1204 <omahirkar008@gmail.com>
2023-01-11 22:07:42 +0530

    Method Calling

--- bootcoding_java/src/com/bootcoding/myexample/ExerciseScanner.java
+++ bootcoding_java/src/com/bootcoding/myexample/ExerciseScanner.java
@@ -7,22 +7,12 @@ public class ExerciseScanner {
         Scanner sc = new Scanner(System.in);
         System.out.println(" Enter A Number.. ");
         int a = sc.nextInt();
-
         System.out.println(" Enter A Number.. ");
         int b = sc.nextInt();
-
-
         System.out.println(" a+b = " + (a+b));
-
         System.out.println(" a2+b2 = " + ((a*a)+(b*b)));
-
-
         System.out.println(" (a+b)2 = " + ((a+b)*(a+b)));
-
-
         System.out.println(" a2+2ab+b2 = " + ((a*a)+(2*a*b)+(b*b)));
-
-
         System.out.println(" a2b2+2ab+a2-b2 = " + ((a*a)+(b*b)+(2*a*b)+(a*a)-(b*b)));
     }
 }
