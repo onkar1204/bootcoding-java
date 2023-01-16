4a8eb7162fae24b80bc37363ad369c499dbf6a26
omkar1204 <omahirkar008@gmail.com>
2023-01-16 23:33:05 +0530

    recursion Examples

--- bootcoding_java/src/com/bootcoding/myexample/RecursionExample1.java
+++ bootcoding_java/src/com/bootcoding/myexample/RecursionExample1.java
@@ -1,25 +1,21 @@
 package com.bootcoding.myexample;
-
+// WAP to sum of numbers from 1 to n using recursion
 public class RecursionExample1 {
     public int printNumber(int num){
-
         //base condition or terminate condition
         if(num == 1){
             return 1;
         }
-
         int res = num + printNumber(num-1);
 //        System.out.println(res);
        return res ;
     }
     public static void main(String[] args) {
-        int num = 10;
+        int num = 20;
         RecursionExample1 recursionExample = new RecursionExample1();
         System.out.println( recursionExample.printNumber(num));
-
     }
     // 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 +
-
 
     // [10,20,30]
     // contageous memory location
