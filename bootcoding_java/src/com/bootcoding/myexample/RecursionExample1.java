01f12c6433ae79dd25f4a18f7fc2b91aca99449d
omkar1204 <omahirkar008@gmail.com>
2023-01-16 22:49:46 +0530

    recursion Examples

---
+++ bootcoding_java/src/com/bootcoding/myexample/RecursionExample1.java
@@ -0,0 +1,29 @@
+package com.bootcoding.myexample;
+
+public class RecursionExample1 {
+    public int printNumber(int num){
+
+        //base condition or terminate condition
+        if(num == 1){
+            return 1;
+        }
+
+        int res = num + printNumber(num-1);
+//        System.out.println(res);
+       return res ;
+    }
+    public static void main(String[] args) {
+        int num = 10;
+        RecursionExample1 recursionExample = new RecursionExample1();
+        System.out.println( recursionExample.printNumber(num));
+
+    }
+    // 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 +
+
+
+    // [10,20,30]
+    // contageous memory location
+    // data value
+    // [data, next] -> [data, next]
+    // [10, address of next element] -> [20, address of next eleemnt] -> [30, null]
+}
