1389ab8e96e3ac3c831e274e3ccaea34eca70227
omkar1204 <omahirkar008@gmail.com>
2023-01-16 23:33:12 +0530

    recursion Examples

--- bootcoding_java/src/com/bootcoding/myexample/RecursionExample.java
+++ bootcoding_java/src/com/bootcoding/myexample/RecursionExample.java
@@ -1,22 +1,16 @@
 package com.bootcoding.myexample;
-
+//WAP to print first 50 natural numbers using recursion (1,2,3,4....)
 public class RecursionExample {
-
-    public void test(int num) {
-
+    public void printNatural(int num) {
         if(num == 0) {
             return;
         }
-
-        test(num - 1);
-
+        printNatural(num - 1);
         System.out.println(num);
     }
     public static void main(String[] args) {
-        int num = 100-50;
-
+        int num = 50;
         RecursionExample recursionExample = new RecursionExample();
-        recursionExample.test(num);
-
+        recursionExample.printNatural(num);
     }
 }
