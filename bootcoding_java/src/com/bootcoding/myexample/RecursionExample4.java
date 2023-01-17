1eadbf25d3bf8d9c8d166387e3aae5dbd870730d
omkar1204 <omahirkar008@gmail.com>
2023-01-17 15:14:57 +0530

    recursion Examples

---
+++ bootcoding_java/src/com/bootcoding/myexample/RecursionExample4.java
@@ -0,0 +1,20 @@
+package com.bootcoding.myexample;
+//WAP to count the digits of a given number using recursion
+public class RecursionExample4 {
+   static int count = 0;
+public static int countDigits(int num) {
+
+    if (num != 0) {
+        count = count+1;
+        countDigits(num / 10);
+
+    }
+    return count;
+}
+
+    public static void main(String[] args) {
+        int num = 12345678;
+
+          System.out.println(countDigits(num));
+    }
+}
