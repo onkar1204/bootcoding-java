4d3d3c964cab43477a4f7fd84993c0e3a90a8808
omkar1204 <omahirkar008@gmail.com>
2023-01-17 17:29:36 +0530

    recursion Examples

--- bootcoding_java/src/com/bootcoding/myexample/RecursionExample3.java
+++ bootcoding_java/src/com/bootcoding/myexample/RecursionExample3.java
@@ -2,13 +2,12 @@ package com.bootcoding.myexample;
 // WAP to print the array elements using recursion
 public class RecursionExample3 {
 
-    public int arrayElements(int num){
+    public void arrayElements(int num){
         if (num == 0){
-            return 1;
+            return ;
         }
         arrayElements(num-1);
         System.out.println(num);
-        return num;
     }
 
     public static void main(String[] args) {
