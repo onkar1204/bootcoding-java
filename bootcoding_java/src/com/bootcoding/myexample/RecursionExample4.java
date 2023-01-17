dbae2e6df6c8a34a667e1916ef1f9d1e87091fec
omkar1204 <omahirkar008@gmail.com>
2023-01-17 17:31:33 +0530

    recursion Examples

--- bootcoding_java/src/com/bootcoding/myexample/RecursionExample4.java
+++ bootcoding_java/src/com/bootcoding/myexample/RecursionExample4.java
@@ -3,15 +3,12 @@ package com.bootcoding.myexample;
 public class RecursionExample4 {
    static int count = 0;
 public static int countDigits(int num) {
-
     if (num != 0) {
         count = count+1;
         countDigits(num / 10);
-
     }
     return count;
 }
-
     public static void main(String[] args) {
         int num = 12345678;
 
