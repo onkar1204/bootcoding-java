d27eab43fc1793dc04bb3598394f237445117019
omkar1204 <omahirkar008@gmail.com>
2023-01-19 17:28:20 +0530

    recursion Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/EvenDigitFind.java
@@ -0,0 +1,24 @@
+package com.bootcoding.dsa;
+//Find Numbers with Even Number of Digits
+class EvenDigitFind {
+    public static void main(String[] args) {
+        int[] nums ={12,345,2,6,7896};
+         findNumbers(nums);
+    }
+    public static void findNumbers(int[] nums) {
+        int sum = 0;
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            int count = 0;
+            while(nums[i] != 0){
+                int div = nums[i]/10;
+                count++;
+                nums[i]=div;
+            }
+            if (count % 2 == 0){
+                sum++;
+            }
+        }
+        System.out.println(sum);
+    }
+}
