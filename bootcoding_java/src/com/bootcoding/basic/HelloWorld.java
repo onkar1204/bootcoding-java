2bb09bfb6bf13639ed68b08a75accf6136be0277
onkar1204 <onkarahirkar@gmail.com>
2023-01-19 17:59:42 +0530

    recursion Examples

--- bootcoding_java/src/com/bootcoding/basic/HelloWorld.java
+++ bootcoding_java/src/com/bootcoding/basic/HelloWorld.java
@@ -2,6 +2,6 @@ package com.bootcoding.basic;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello World");
     }
 }
