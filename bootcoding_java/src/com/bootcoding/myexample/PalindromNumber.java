6ed1caa4f629536eeb227e637abcabb6c1d6e1ca
onkar1204 <onkarahirkar@gmail.com>
2023-01-23 16:50:56 +0530

    recursion Examples

---
+++ bootcoding_java/src/com/bootcoding/myexample/PalindromNumber.java
@@ -0,0 +1,30 @@
+package com.bootcoding.myexample;
+
+public class PalindromNumber {
+    public static void main(String args[]){
+
+    int rem = 0;
+
+    int n = 239;
+
+    int temp = n;
+
+        while(n != 0){
+
+    int div = n/10;
+
+        rem = rem * 10 + n%10;
+
+        n = div;
+    }
+        if(rem == temp){
+	
+        System.out.println( rem + " Is   Palindrom");
+
+    }else{
+	
+        System.out.println( rem + " Is Not Palindrom");
+
+        }
+    }
+}
