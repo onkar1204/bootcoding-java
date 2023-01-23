4a486b03a35a65f711676ba1b62e58ad1cd4750c
onkar1204 <onkarahirkar@gmail.com>
2023-01-23 17:07:12 +0530

    recursion Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/ReverseInteger.java
@@ -0,0 +1,33 @@
+package com.bootcoding.dsa;
+
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        int x =231;
+        int res = reverse(x);
+        System.out.println(res);
+
+    }
+    public static int reverse(int x) {
+
+        int div = 0;
+        int rem =0;
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        if (x> Integer.MAX_VALUE || x<  Integer.MIN_VALUE){
+            return 0;
+        }
+
+        while(x != 0){
+
+            div = x/10;
+
+            rem = rem * 10 + x%10;
+
+            x = div;
+        }
+        return rem;
+    }
+}
