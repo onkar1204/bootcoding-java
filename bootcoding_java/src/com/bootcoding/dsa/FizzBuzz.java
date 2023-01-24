6d9bc3297456afb761242bc842b622a64e3f5a3c
onkar1204 <onkarahirkar@gmail.com>
2023-01-24 16:50:49 +0530

    leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/FizzBuzz.java
+++ bootcoding_java/src/com/bootcoding/dsa/FizzBuzz.java
@@ -10,7 +10,6 @@ public class FizzBuzz {
             }
             else if ( i % 3 == 0) {
                 System.out.println("Fizz");
-                
             } else if (i % 5 == 0) {
                 System.out.println("Buzz");
             }else{
