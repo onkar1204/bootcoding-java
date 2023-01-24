ea8e54c75d1e7b40e03a873089558e1a773bbf65
onkar1204 <onkarahirkar@gmail.com>
2023-01-24 16:51:02 +0530

    leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/FizzBuzz.java
+++ bootcoding_java/src/com/bootcoding/dsa/FizzBuzz.java
@@ -2,7 +2,6 @@ package com.bootcoding.dsa;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-
         int n = 15;
         for(int i=1; i<=n; i++){
             if (i % 3 == 0  &&  i % 5 ==0 ){
