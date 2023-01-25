e7cda48e3ce36ee421966ddbd87f6f54f201e315
onkar1204 <onkarahirkar@gmail.com>
2023-01-25 15:37:04 +0530

    leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/CommonDivisor.java
+++ bootcoding_java/src/com/bootcoding/dsa/CommonDivisor.java
@@ -6,7 +6,6 @@ public class CommonDivisor {
 
     public static void main(String[] args) {
         int[] nums={3,3};
-
         System.out.println(findGCD(nums));
         }
     public static int findGCD(int[] nums) {
