9e1551c43d0f60c1b693ac47348e01d4015f980f
onkar1204 <onkarahirkar@gmail.com>
2023-01-26 17:10:42 +0530

    leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/AddDigits.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa;
+
+public class AddDigits{
+
+    public static int addDigits(int num) {
+        if (num < 10){
+            return num;
+        }
+        return addDigits(num/10 + num%10);
+    }
+
+    public static void main(String[] args) {
+            int num = 38;
+
+        System.out.println(addDigits(num));
+
+
+        }
+    }
+
+    //18, 234, 123, 72
