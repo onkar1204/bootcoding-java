bea0994a5251fd5f8b7dca7da6b04eccf31355e6
onkar1204 <onkarahirkar@gmail.com>
2023-01-26 15:05:06 +0530

    leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/TwoSum.java
@@ -0,0 +1,30 @@
+package com.bootcoding.dsa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15}; int target = 9;
+//        Output: [0,1]
+        for (Integer rs: twoSum(nums,target)){
+            System.out.println(rs);
+        }
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int[] arr = new int[2];
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                arr[0] =  map.get(nums[i]);
+                arr[1] = i;
+                return arr;
+            }
+            else{
+                map.put(target-nums[i] , i);
+            }
+        }
+        return arr;
+    }
+}
