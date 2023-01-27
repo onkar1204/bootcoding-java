0a51fcb60f1997120a77229cf6b891723032e414
onkar1204 <onkarahirkar@gmail.com>
2023-01-27 16:12:30 +0530

    leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/MoveZeroes.java
+++ bootcoding_java/src/com/bootcoding/dsa/MoveZeroes.java
@@ -6,7 +6,6 @@ public class MoveZeroes {
         int[] nums = {0,1,0,3,12};
 
         int x =0;
-
         for (int i = 0; i < nums.length; i ++) {
             if (nums[i] != 0) {
                 nums[x] = nums[i];
