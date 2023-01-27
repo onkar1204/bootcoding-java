27d17dd6f44afe5dde05daff2fbe2886f9f10f05
onkar1204 <onkarahirkar@gmail.com>
2023-01-27 16:21:06 +0530

    leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/MoveZeroes.java
+++ bootcoding_java/src/com/bootcoding/dsa/MoveZeroes.java
@@ -1,21 +1,27 @@
 package com.bootcoding.dsa;
+
 // Move Zeroes
 public class MoveZeroes {
-//    Output: [1,3,12,0,0]
+    //    Output: [1,3,12,0,0]
     public static void main(String[] args) {
-        int[] nums = {0,1,0,3,12};
+        int[] nums = {0, 1, 0, 3, 12};
 
-        int x =0;
-        for (int i = 0; i < nums.length; i ++) {
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[x] = nums[i];
                 x++;
-                System.out.println(nums[i]);
+
             }
         }
-            for (int j=x; j< nums.length; j++) {
-                nums[j] = 0;
-                System.out.println(nums[j]);
+        for (int j = x; j < nums.length; j++) {
+            nums[j] = 0;
+
+
+        }
+
+        for (int k=0; k< nums.length; k++){
+            System.out.println(nums[k]);
         }
     }
 }
