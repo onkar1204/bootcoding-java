d38f3c7a3d83658a1231149128ab6c1f528719c5
onkar1204 <onkarahirkar@gmail.com>
2023-01-30 17:15:15 +0530

    Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,29 @@
+package com.bootcoding.dsa;
+
+public class FinalValueOfVariableAfterPerformingOperations {
+    public static void main(String[] args) {
+        String[] operations = {"X++","++X","--X","X--"};
+        System.out.println(finalValueAfterOperations(operations));
+    }
+
+    public static int finalValueAfterOperations(String[] operations) {
+
+        int x = 0;
+
+        for (int i=0; i<operations.length; i++){
+
+            if (operations[i].equals("--X")){
+                x = x-1;
+            } else if (operations[i].equals("X++")) {
+                x = x+1;
+            } else if (operations[i].equals("++X")) {
+                x = x+1;
+            }else if (operations[i].equals("X--")){
+                x=x-1;
+            }
+
+        }
+
+        return x;
+    }
+}
