32085f34852a0127e9860b668ed7c47174fd835d
onkar1204 <onkarahirkar@gmail.com>
2023-01-30 17:18:04 +0530

    Examples

--- bootcoding_java/src/com/bootcoding/dsa/DifferenceBetweenElementSumAndDigitSumOfAnArray.java
+++ bootcoding_java/src/com/bootcoding/dsa/DifferenceBetweenElementSumAndDigitSumOfAnArray.java
@@ -6,7 +6,7 @@ public class DifferenceBetweenElementSumAndDigitSumOfAnArray {
                 51,77,36,38,81,38,69,60,29,21,66,6,62,55,13,90,66,7,15,15,60,76,44,30,6,86,
                 87,59,88,36,32,35,67,13,79,43,27,2,97,41,4,44,91,11,5,48,38,64,9,90,39,28,
                 50,57,60,4,99,44,39,12,95,32,66,100,45,42,22,35,65,7,49,43,41,40,64,78};
-//        Output: 9
+
         System.out.println(differenceOfSum(nums));
     }
     public static int differenceOfSum(int[] nums) {
