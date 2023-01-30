7e88b4368c22917d976e72dddfc6fb19c9d0f357
onkar1204 <onkarahirkar@gmail.com>
2023-01-30 19:18:53 +0530

    Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/NumberOfGoodPairs.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa;
+
+public class NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+        //Output: 4
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i=0; i<nums.length; i++){
+            for (int j=i+1; j<nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
