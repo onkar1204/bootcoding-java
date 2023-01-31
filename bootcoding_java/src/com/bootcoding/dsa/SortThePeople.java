570c5ba8de09762b8f7244d07dbff78f7a12dbda
onkar1204 <onkarahirkar@gmail.com>
2023-01-31 15:35:39 +0530

    Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/SortThePeople.java
@@ -0,0 +1,32 @@
+package com.bootcoding.dsa;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SortThePeople {
+    public static void main(String[] args) {
+        String[] names = {"Mary","John","Emma"};
+        int[] heights = {180,165,170};
+        //Output: ["Mary","Emma","John"]
+        for (String st :sortPeople(names,heights) ){
+            System.out.println(st);
+        }
+    }
+    public static String[] sortPeople(String[] names, int[] heights) {
+       Map<Integer , String> map = new HashMap<>();
+
+       for (int i=0; i< names.length; i++){
+           map.put(heights[i] ,names[i]);
+           }
+        Arrays.sort(heights);
+       String[] res = new String[names.length];
+        int index = 0;
+       for (int a= heights.length-1; a>=0; a--){
+
+           res[index] = map.get(heights[a]);
+           index++;
+       }
+       return res;
+    }
+}
