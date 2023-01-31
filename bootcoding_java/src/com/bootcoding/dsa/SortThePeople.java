e7e765efa8a28eb24524dfbdb14a5d2eadaae9d9
onkar1204 <onkarahirkar@gmail.com>
2023-01-31 15:36:12 +0530

    Examples

--- bootcoding_java/src/com/bootcoding/dsa/SortThePeople.java
+++ bootcoding_java/src/com/bootcoding/dsa/SortThePeople.java
@@ -15,7 +15,6 @@ public class SortThePeople {
     }
     public static String[] sortPeople(String[] names, int[] heights) {
        Map<Integer , String> map = new HashMap<>();
-
        for (int i=0; i< names.length; i++){
            map.put(heights[i] ,names[i]);
            }
