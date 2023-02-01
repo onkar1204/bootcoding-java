9a9d9c7afba8bf3a72f3dbcbf591a30bd5119aeb
onkar1204 <onkarahirkar@gmail.com>
2023-02-01 17:37:54 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/SearchinRotatedSortedArray.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa;
+
+public class SearchinRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+      //  Output: 4
+        System.out.println(search(nums , target));
+
+    }
+    public static int search(int[] nums, int target) {
+
+        for(int i=0; i<nums.length; i++){
+            if(target == nums[i]){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
