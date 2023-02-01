02f101c512d6fbc9c31211103c8c57970661dd2c
onkar1204 <onkarahirkar@gmail.com>
2023-02-01 17:33:32 +0530

    Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/BinarySearch.java
@@ -0,0 +1,19 @@
+package com.bootcoding.dsa;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        //Output: 4
+        System.out.println(search(nums , target));
+    }
+    public static int search(int[] nums, int target) {
+        int res = -1;
+        for(int i=0; i<nums.length; i++){
+            if(target == nums[i]){
+                res = i;
+            }
+        }
+        return res;
+    }
+}
