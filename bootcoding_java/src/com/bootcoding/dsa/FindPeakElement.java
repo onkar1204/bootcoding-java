5e357f85e0570b56b881b865db23ca5504517d0c
onkar1204 <onkarahirkar@gmail.com>
2023-02-02 19:58:45 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/FindPeakElement.java
@@ -0,0 +1,21 @@
+package com.bootcoding.dsa;
+
+public class FindPeakElement {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+//        Output: 2
+        System.out.println(findPeakElement(nums));
+    }
+
+    public static int findPeakElement(int[] nums) {
+        int max = nums[0];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
