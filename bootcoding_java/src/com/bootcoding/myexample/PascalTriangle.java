97d15c8a3a60d11e0987057d4b6ae48d6bcf31ec
onkar1204 <onkarahirkar@gmail.com>
2023-02-02 20:03:35 +0530

    Leetcode Examples

--- bootcoding_java/src/com/bootcoding/myexample/PascalTriangle.java
+++ bootcoding_java/src/com/bootcoding/myexample/PascalTriangle.java
@@ -7,7 +7,6 @@ public static void main (String[] args){
 	int count = 1;
 	for(int i=0; i<n; i++){
 		for(int j=n-i; j>=1; j--) {
-
 			System.out.print(" ");
 		}
 		for (int l=0; l<=i; l++){
@@ -17,14 +16,9 @@ public static void main (String[] args){
 			}else{
 				count=count*(i-l+1)/l; // 1. Parenthesis, 2.substraction, 3. Addition, 4. Multiplication, 5.Division
 			}
-
 			System.out.print(count + " ");
 		}
-
-
 		System.out.println();
-
-
 	}
 }
 }	
