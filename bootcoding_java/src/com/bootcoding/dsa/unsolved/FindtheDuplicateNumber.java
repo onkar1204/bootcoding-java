81dc1badde04ccba3a33f538908ec751056c4ea5
onkar1204 <onkarahirkar@gmail.com>
2023-02-02 19:56:31 +0530

    Leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/unsolved/FindtheDuplicateNumber.java
+++ bootcoding_java/src/com/bootcoding/dsa/unsolved/FindtheDuplicateNumber.java
@@ -13,7 +13,6 @@ public class FindtheDuplicateNumber {
     public static int findDuplicate(int[] nums) {
         Map<Integer,Integer> map = new HashMap<>();
 
-
            for (int i = 0; i < nums.length; i++) {
                if(!map.containsKey(nums[i])){
                    map.put(nums[i], 1);
