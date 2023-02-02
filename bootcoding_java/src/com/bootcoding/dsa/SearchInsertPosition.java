61cbd6a273066be638f2fb0aa55890bff4e8ecf7
onkar1204 <onkarahirkar@gmail.com>
2023-02-02 19:57:49 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/SearchInsertPosition.java
@@ -0,0 +1,24 @@
+package com.bootcoding.dsa;
+
+import java.util.Arrays;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] nums = {3,6,7,8,10};
+        int target = 5;
+//        Output: 2
+        System.out.println(searchInsert(nums , target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int res = nums.length;
+        for(int i=0; i<nums.length; i++){
+            if(target == nums[i]){
+                return i;
+            } else if (target < nums[i]) {
+                return i;
+            }
+        }
+        return res;
+        }
+}
