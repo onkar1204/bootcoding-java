c3bbc545e9076f47e1c39cca7f5557ca0627936f
onkar1204 <onkarahirkar@gmail.com>
2023-02-03 15:06:39 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/unsolved/CountNumberofPairsWithAbsoluteDifference.java
@@ -0,0 +1,27 @@
+package com.bootcoding.dsa.unsolved;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountNumberofPairsWithAbsoluteDifference {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,1};
+        int k = 1;
+//        Output: 4
+        System.out.println(countKDifference(nums,k));
+    }
+    public static int countKDifference(int[] nums, int k) {
+
+        int count = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            for (int j = i+1; j <= nums.length-1; j++) {
+                if (Math.abs(nums[i]-nums[j]) == k) {
+
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
