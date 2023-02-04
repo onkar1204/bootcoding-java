b2a98c837ceda878f8dce83cbe1cd1d220c03a45
onkar1204 <onkarahirkar@gmail.com>
2023-02-04 17:49:50 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/HeightChecker.java
@@ -0,0 +1,43 @@
+package com.bootcoding.dsa;
+
+import java.util.Arrays;
+
+public class HeightChecker {
+    public static void main(String[] args) {
+        int[] heights = {1, 1, 4, 2, 1, 3};
+//        Output: 3
+        System.out.println(heightChecker(heights));
+    }
+
+    public static int heightChecker(int[] heights) {
+        int[] arr = new int[heights.length];
+        for (int a = 0; a < heights.length; a++) {
+            arr[a] = heights[a];
+
+
+
+
+
+
+        }
+        Arrays.sort(arr);
+        int count = 0;
+
+
+
+
+
+        int c1 = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != arr[i]) {
+                c1++;
+//                }else if (heights[i] != arr[i]){
+//                    count++;
+//                }
+
+            }
+
+
+        }return c1;
+    }
+}
