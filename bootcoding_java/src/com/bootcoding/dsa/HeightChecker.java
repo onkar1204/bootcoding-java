049499673784e20c60158454cea1c2b5df4c86f4
onkar1204 <onkarahirkar@gmail.com>
2023-02-04 17:53:57 +0530

    Leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/HeightChecker.java
+++ bootcoding_java/src/com/bootcoding/dsa/HeightChecker.java
@@ -17,7 +17,6 @@ public class HeightChecker {
         Arrays.sort(arr);
         int count = 0;
 
-
         for (int i = 0; i < heights.length; i++) {
             if (heights[i] != arr[i]) {
                 count++;
