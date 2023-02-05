829071d4fff3d35b568ceb3b9566da402486e6f0
onkar1204 <onkarahirkar@gmail.com>
2023-02-05 15:34:49 +0530

    Leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/FindPeakElement.java
+++ bootcoding_java/src/com/bootcoding/dsa/FindPeakElement.java
@@ -3,7 +3,7 @@ package com.bootcoding.dsa;
 public class FindPeakElement {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 1};
-//        Output: 2
+//        Output: 3
         System.out.println(findPeakElement(nums));
     }
     public static int findPeakElement(int[] nums) {
