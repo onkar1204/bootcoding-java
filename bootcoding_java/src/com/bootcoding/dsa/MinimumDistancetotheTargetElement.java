222b426ed38b63964785a1b604f9399f56ff0df9
onkar1204 <onkarahirkar@gmail.com>
2023-02-06 18:32:33 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/MinimumDistancetotheTargetElement.java
@@ -0,0 +1,20 @@
+package com.bootcoding.dsa;
+
+public class MinimumDistancetotheTargetElement {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,1,1,1,1,1,1,1};
+        int target = 1;
+        int start = 9;
+//        Output: 0
+        System.out.println(getMinDistance(nums , target , start));
+    }
+    public static int getMinDistance(int[] nums, int target, int start) {
+        int res = 1000000;
+        for(int i=0; i<nums.length; i++){
+            if(target == nums[i]){
+                res = Math.min(res, Math.abs(i - start));
+            }
+        }
+        return res;
+    }
+}
