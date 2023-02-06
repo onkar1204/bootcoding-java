2a1a0b9ddfca26630d04ddcc059bdf7ca3950519
onkar1204 <onkarahirkar@gmail.com>
2023-02-06 18:32:17 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/Sqrt.java
@@ -0,0 +1,22 @@
+package com.bootcoding.dsa;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        int x = 4;
+//        Output: 2
+        System.out.println(mySqrt(x));
+    }
+    public static int mySqrt(int x) {
+        double temp;
+        double sqroot = x/2;
+        if(x == 0 || x == 1){
+            return x;
+        }
+        do{
+            temp = sqroot;
+            sqroot = (temp+(x/temp))/2;
+        }
+        while(temp - sqroot != 0);
+        return (int)Math.floor(sqroot);
+    }
+}
