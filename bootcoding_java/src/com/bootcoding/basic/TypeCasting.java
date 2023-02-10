4e0215330c5df78c8618bcd5166af08dd476f2fe
onkar1204 <onkarahirkar@gmail.com>
2023-02-10 15:05:47 +0530

    Examples

---
+++ bootcoding_java/src/com/bootcoding/basic/TypeCasting.java
@@ -0,0 +1,22 @@
+package com.bootcoding.basic;
+
+
+
+
+
+
+
+
+
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        double  a = 10.5;
+        byte b = (byte) a;
+        int c = (int) a;
+
+        System.out.println("After convert double data type to byte : "+b);
+        System.out.println("After convert double data type to int  : "+c);
+    }
+}
