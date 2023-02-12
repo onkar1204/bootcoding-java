35deeb077f63f912254514533984485ece37ee3c
onkar1204 <onkarahirkar@gmail.com>
2023-02-12 14:40:17 +0530

    Examples

--- bootcoding_java/src/com/bootcoding/basic/YearsToDays.java
+++ bootcoding_java/src/com/bootcoding/basic/YearsToDays.java
@@ -6,7 +6,6 @@ public class YearsToDays {
         return days;
     }
     public static void main(String[] args) {
-
         int years =2;
         int days = convert(years);
         System.out.println ( years + " years " + " is " + days + " days "  );
