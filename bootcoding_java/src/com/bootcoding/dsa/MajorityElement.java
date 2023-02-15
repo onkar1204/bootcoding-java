ea9a81b2a5104b1a9909a6f7620dc127d74d8f8e
onkar1204 <onkarahirkar@gmail.com>
2023-02-15 16:14:10 +0530

    Leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/MajorityElement.java
+++ bootcoding_java/src/com/bootcoding/dsa/MajorityElement.java
@@ -6,7 +6,6 @@ import java.security.Key;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-
 public class MajorityElement {
     public static void main(String[] args) {
         int[] nums = {2,2,1,1,1,2,2};
