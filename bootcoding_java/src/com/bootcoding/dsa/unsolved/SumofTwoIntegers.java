8d2fb4e01c4811868deacd88f2d83f16e034b0cc
onkar1204 <onkarahirkar@gmail.com>
2023-03-23 18:18:26 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/unsolved/SumofTwoIntegers.java
@@ -0,0 +1,13 @@
+package com.bootcoding.dsa.unsolved;
+
+public class SumofTwoIntegers {
+    public static void main(String[] args) {
+        int a = 1, b = 2;
+//        Output: 3
+        System.out.println(getSum(a,b));
+    }
+
+    public static int getSum(int a, int b) {
+       return Integer.sum(a,b);
+    }
+}
