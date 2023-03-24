dcc1a01e7ad88bbfc70cd5ad8129acf15c1e6c82
onkar1204 <onkarahirkar@gmail.com>
2023-03-24 15:46:13 +0530

    Leetcode Examples

--- bootcoding_java/src/com/bootcoding/dsa/BinarySearch.java
+++ bootcoding_java/src/com/bootcoding/dsa/BinarySearch.java
@@ -1,6 +1,6 @@
 package com.bootcoding.dsa;
 public class BinarySearch {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         int[] nums = {-1,0,3,5,9,12};
         int target = 9;
         //Output: 4
@@ -14,5 +14,26 @@ public class BinarySearch {
             }
         }
         return res;
-    }
+    }*/
+        public static void main(String[] args) {
+            int [] a={2,3,4,5,6,7};
+            int x=6;
+            int index = binarySearch(a, x, 0, a.length - 1);
+            System.out.println(x + " found in " + index+" index..");
+        }
+        private  static int binarySearch(int a[],int x,int left,int right) {
+            int mid=(left+right)/2;
+            if (x==a[mid]) {
+                return mid;
+            }
+            if (left>=right) {
+                return -1;
+            }
+            if (x > a[mid]) {
+                return binarySearch(a, x, mid+1, right);
+            }
+            else {
+                return binarySearch(a, x, left, mid-1);
+            }
+        }
 }
