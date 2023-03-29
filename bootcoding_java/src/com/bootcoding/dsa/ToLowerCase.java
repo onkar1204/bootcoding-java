cec769fa5d60ed759c69363b370458677af07a12
onkar1204 <onkarahirkar@gmail.com>
2023-03-29 14:25:06 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/ToLowerCase.java
@@ -0,0 +1,14 @@
+package com.bootcoding.dsa;
+//709. To Lower Case
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String s = "Hello";
+    //    Output: "hello"
+        System.out.println(toLowerCase(s));
+    }
+    public static String toLowerCase(String s) {
+
+        String str =(s.toLowerCase());
+        return str;
+    }
+}
