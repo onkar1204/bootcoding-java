c8812a3862f9bce598a7edd954ccee08eb8ed570
onkar1204 <onkarahirkar@gmail.com>
2023-03-29 14:06:03 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/basic/countlength.java
@@ -0,0 +1,13 @@
+package com.bootcoding.basic;
+
+public class countlength {
+    public static void main(String[] args) {
+        int num = 252525255;
+        int count = 0;
+    while (num != 0){
+        num = num / 10;
+         count++;
+        }
+        System.out.println(count);
+    }
+}
