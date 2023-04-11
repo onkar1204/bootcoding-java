358c02709b29c8d2475e737315f0c5da8710b898
onkar1204 <onkarahirkar@gmail.com>
2023-04-11 12:28:44 +0530

    Leetcode Examples

---
+++ bootcoding_java/src/com/bootcoding/dsa/CheckifNumbersAreAscendinginaSentence.java
@@ -0,0 +1,27 @@
+package com.bootcoding.dsa;
+//2042. Check if Numbers Are Ascending in a Sentence
+public class CheckifNumbersAreAscendinginaSentence {
+    public static void main(String[] args) {
+        String s = "1 box has 3 blue 4 red 6 green and 12 yellow marbles";
+//        Output: true
+        System.out.println(areNumbersAscending(s));
+    }
+    public static boolean areNumbersAscending(String s) {
+        int pre = 0;
+        for (int i = 0; i < s.length(); i++) {
+            String[] str = s.split(" ");
+            try {
+                int nums = Integer.parseInt(str[i]);
+                if (nums <= pre){
+                    return false;
+                }
+                pre=nums;
+                System.out.println(str[i]);
+            }
+            catch (Exception e){
+
+            }
+        }
+    return true;
+    }
+}
