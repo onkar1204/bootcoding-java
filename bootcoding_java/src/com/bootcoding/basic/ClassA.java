c872a2cafa7475099e4fc80c22b3261e0bf1bf26
onkar1204 <114867129+onkar1204@users.noreply.github.com>
2023-04-22 13:06:36 +0530

    Update ClassA.java

--- bootcoding_java/src/com/bootcoding/basic/ClassA.java
+++ bootcoding_java/src/com/bootcoding/basic/ClassA.java
@@ -1,20 +1,7 @@
 package com.bootcoding.basic;
 public class ClassA {
     int a = 10;
-    int b = a;
-    
-    
-    
-    
- 
-    
-    
-    
-    
-    
-    
-
-    
+    int b = a; 
     
 }
 
