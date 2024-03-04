5c08b556c2cb368cc62e8ee531b019e754908ce2
onkar1204 <114867129+onkar1204@users.noreply.github.com>
2024-03-04 15:42:25 +0530

    Update BubleSort.java

--- bootcoding_java/src/com/bootcoding/sorting/BubleSort.java
+++ bootcoding_java/src/com/bootcoding/sorting/BubleSort.java
@@ -7,7 +7,6 @@ public class BubleSort {
         for (int i = 0; i < n - 1; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (a[j] < a[i]) {
-
                     int temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
